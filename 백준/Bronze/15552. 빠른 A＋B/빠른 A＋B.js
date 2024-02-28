835da0fb519c47fa1c4de2cf2fd9525e40a6e918
hong3737 <110447235+hong3737@users.noreply.github.com>
2024-02-28 11:16:23 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1332 ms, Memory: 166040 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+let result = "";
+
+for (let i = 1; i <= +input[0]; i++) {
+    let sum = typeof input[i] === "string" ? input[i].trim().split(" ") : " ";
+    result += +sum[0] + +sum[1] + "\n";
+}
+
+console.log(result);
