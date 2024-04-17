20a2375517b82c8f8f5c7f80c11fdad10cd39346
hong3737 <110447235+hong3737@users.noreply.github.com>
2024-04-17 15:38:21 +0900

    [level 0] Title: 모스부호 (1), Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    let result ="";
+    const morse_letter = letter.split(" ");
+    const morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    
+    for(const item of morse_letter){
+        result += morse[item];
+    }
+    return result;
+}
