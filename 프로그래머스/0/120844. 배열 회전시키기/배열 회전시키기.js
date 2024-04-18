c955b8ccdec670a942c909f35cb597cb5a32f751
hong3737 <110447235+hong3737@users.noreply.github.com>
2024-04-18 12:19:57 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,10 @@
+function solution(numbers, direction) {
+    if(direction === "left"){
+        numbers.push(numbers[0]);
+        numbers.shift();
+    }else {
+        numbers.unshift(numbers[numbers.length-1]);
+        numbers.pop();
+    }
+    return numbers;
+}
