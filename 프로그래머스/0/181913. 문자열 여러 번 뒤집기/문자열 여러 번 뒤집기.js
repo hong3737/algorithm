21a24cc766ab2a27214b6815418867ef8a96e973
hong3737 <110447235+hong3737@users.noreply.github.com>
2024-08-28 09:22:13 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 14.82 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,9 @@
+function solution(my_string, queries) {
+    for(let i=0; i<queries.length; i++){
+        const [start, end] = queries[i];
+        const reverse = [...my_string].slice(start, end+1).reverse().join("");
+        
+        my_string = my_string.slice(0,start) + reverse + my_string.slice(end+1);
+    }
+    return my_string;
+}
